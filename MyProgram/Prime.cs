a79d1d72551fb100d1e26a092739b99acfc2a0c1
terrymccarthy3000 <terry_mccarthy@outlook.com>
2016-03-01 16:45:32 -0600

    <Terry> Created Prime class and Unit Test project
    Created Prime class and Unit Test project
    TODO
    Factor out core logic into specialized classes with methods that return
    objects (like lists).
    Create UnitTest for each method.

---
+++ MyProgram/Prime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProgram
+{
+	public class Prime
+	{
+		/// <summary>
+		/// Get a list of the first 'n' prime numbers
+		/// </summary>
+		/// <param name="n"></param>
+		/// <returns></returns>
+		public List<int> GetPrimeList(int n)
+		{
+			//Liang TODO: implement
+			throw new NotImplementedException();
+		}
+
+	}
+}
---
+++ MyProgramUnitTests/PrimeTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using MyProgram;
+
+namespace MyProgramUnitTests
+{
+	/// <summary>
+	/// Class to test methods in Prime class
+	/// </summary>
+	[TestClass]
+	public class PrimeTests
+	{
+
+		[TestMethod]
+		public void GetPrimeListReturnsCorrectFirst10PrimeNumbers()
+		{
+			//Arrange 
+			Prime primeObj = new Prime();
+			List<int> expectedList = new List<int>() { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
+			List<int> actualList;
+
+			//Act
+			actualList = primeObj.GetPrimeList(10);
+			
+			//Assert
+			//Lists are equal length
+			Assert.AreEqual(expectedList.Count, actualList.Count);
+
+			//Lists have the same numbers (Liang TODO)
+			
+		}
+	}
+}
